89578f26e7bda75bf0b639eb44cb9b44b5459b18
ahead123 <arthurallenhead@yahoo.com>
2017-11-10 17:42:21 -0800

    trying ajax call using fetch api instead of axios

--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import { API_URL } from './constants'
+import 'whatwg-fetch';
+import { API_URL } from './constants';
 
 export default class Home extends Component {
   constructor() {
@@ -12,18 +13,9 @@ export default class Home extends Component {
 
   handleInstagramAPIRequest = (event) => {
     event.preventDefault()
-    axios({
-      method: 'GET',
-      url: API_URL,
-      headers: {
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Headers': {
-          'Content-Type': 'application/json'
-        }
-      }
-    })
-    .then(response => console.log('response',response))
-    .catch(error => console.log('error',error))
+    fetch(API_URL)
+      .then(response => console.log(response))
+      .catch(error => console.log(error))
   }
 
 	render() {
