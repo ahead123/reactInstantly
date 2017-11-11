e91779240c6ac7d874caeee7b0ce33bbb94dab8a
ahead123 <arthurallenhead@yahoo.com>
2017-11-10 16:29:36 -0800

    updated redirect uri

--- src/constants.js
+++ src/constants.js
@@ -1,1 +1,1 @@
-export const API_URL = 'https://api.instagram.com/oauth/authorize/?client_id=b02dd812498b4182b234086da2731631&redirect_uri=https://sleepy-brushlands-39585.herokuapp.com/dashboard&response_type=token'
+export const API_URL = 'https://api.instagram.com/oauth/authorize/?client_id=b02dd812498b4182b234086da2731631&redirect_uri=https://sleepy-brushlands-39585.herokuapp.com/&response_type=token'
